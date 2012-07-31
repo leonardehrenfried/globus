936fdb4ab1099f5212e1a8e0d089f1e3b89dd11e
Leonard Ehrenfried <leonard.ehrenfried@web.de>
2012-08-01 00:06:16 +0200

    Add skeleton files and first test

---
+++ src/test/java/PostalCodeValidatorTest.java
@@ -0,0 +1,14 @@
+import info.lenni.globus.PostalCodeValidator;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+public class PostalCodeValidatorTest {
+
+    @Test
+    public void shouldInitialialiseRegexes() throws Exception {
+        assertThat(PostalCodeValidator.postalCodes.size(), is(157));
+    }
+
+}
+
